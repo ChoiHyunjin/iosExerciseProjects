65b7db406d4ee1c9e81110fe1e73ea0b9b2f1f45
choi hyunjin <hyunjin@munch-factory.com>
2022-05-10 12:04:48 +0900

    [편의점 찾기] 프로젝트 생성

---
+++ FindCVS/FindCVS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FindCVS
+//
+//  Created by choi hyunjin on 2022/05/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ FindCVS/README.md
@@ -0,0 +1,6 @@
+# Goal of this project  
+- 지도 api 사용
+- MVVM 패턴 연습
+- RxSwift 연습
+- RxTest 연습
+- Model, ViewModel 테스트
